8284f7ef3116248326ff08f4705e8695c09ed56d
Alejandro Alberto Aguilar Morales <alejandroaguilar1993@outlook.com>
2015-10-29 12:05:51 -0600

    Game Object Classes
    -Renamed Object to Game Object and added Dynamic Game Object.
    
    -Added initial variables to classes

---
+++ DynamicGameObject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace SpaceGame1
+{
+    class DynamicGameObject : GameObject
+    {
+        private Vector2 velocity;
+        private Vector2 acceleration;
+        private float mass;
+
+
+        public Vector2 Acceleration
+        {
+            get { return acceleration; }
+            set { acceleration = value; }
+        }
+
+        public Vector2 Velocity
+        {
+            get { return velocity; }
+            set { velocity = value; }
+        }
+
+        public float Mass
+        {
+            get { return mass; }
+            set { mass = value; }
+        }
+
+
+        public DynamicGameObject(Vector2 origin, Vector2 valVel ,Vector2 valAcc, float valMass): base(origin)
+        {
+            velocity = valVel;
+            acceleration = valAcc;
+            mass = valMass;
+
+        }
+    }
+}
---
+++ GameObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace SpaceGame1
+{
+    class GameObject
+    {
+        private Vector2 position; //maybe protected
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set {position = value; }
+        }
+    
+        public GameObject(Vector2 valPos)
+        {
+            position = valPos;
+        }
+    }
+}
